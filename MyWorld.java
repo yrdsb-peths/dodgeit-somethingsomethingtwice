8403b3d33359642fb2a60649111b55307168ca24
somethingsomethingtwice <341150100@gapps.yrdsb.ca>
2024-04-24 14:28:53 -0400

    added score

--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
-
+    int score = 0;
+    Hero hero;
+    Bomb bomb;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -10,14 +12,20 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        Hero hero = new Hero();
+        super(600, 400, 1);
+        hero = new Hero();
+        bomb = new Bomb();
         addObject(hero, 300, 200);
     }
     
     public void started() {
-        Bomb bomb = new Bomb();
         addObject(bomb, 600, 0);
         bomb.resetBomb();
+    }
+    
+    public void act() {
+        if (hero.getWorld() != null)
+            score++;
+        showText(String.valueOf(score), 300, 32);
     }
 }
